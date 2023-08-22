1a03e50db48a0e2e170f08d093ec8c0a79308b23
EllisRockefeller <ellisyou59@gmail.com>
2023-08-22 07:03:30 +0000

    changed branch

---
+++ connectconfig.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+
+$servername = "localhost:4022";
+$username = "root";
+$password = "Ellis@Monique123";
+$dbname = "istour_and_travel_main";
+
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
